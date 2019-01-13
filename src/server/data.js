f8cb903afcc6f2a832980b3e4c61364aea05e6b2
Salomone Baquis <salomone.l.baquis@gmail.com>
2019-01-12 22:49:44 -0500

    improved data transformation

--- src/server/data.js
+++ src/server/data.js
@@ -2,18 +2,25 @@ const chalk = require("chalk")
 const fs = require("fs")
 const path = require("path")
 const { createFileAndDirectory, errorHandler, fetch, sort } = require("./utils.js")
-const showPath = path.join(__dirname, "..", "..", "data")
+
+const showPath = path.join(__dirname, "../..", "data")
 const showFile = "shows.json"
+const exclusions = [
+  "01CZYDRCJXK43QJ5ZVYM07351A",
+  "1Q7T558F88HRQDH7XCX1854QEM",
+  "1Q7T5GGPJ5ZRN43Y9XYR93FHQ0",
+  "1R4CE5VAEYQ4W68SD30N0R9HQW"
+]
 let promises = []
 
 module.exports = {
   retrieve(page) {
     let link = `https://www.livefromhere.org/listen/${page}.json/`
     return fetch(link).then((response, errorHandler) => {
-      if (response) { 
-        response = JSON.parse(response)              
+      if (response) {
+        response = JSON.parse(response)
         promises.push.apply(promises, response.items)
-        console.log(chalk.blue(`Fetching from ${link}`))        
+        console.log(chalk.blue(`Fetching from ${link}`))
         if (response.next_page) {
           console.log(chalk.yellow(`Next page: ${response.next_page}`))
           return module.exports.retrieve(response.next_page)
@@ -25,16 +32,26 @@ module.exports = {
   transform(shows) {
     return new Promise((resolve, reject) => {
       shows = shows.map(show => {
-        return {
-          id: show.audio.id,
-          date: show.audio.created_at,
-          date_ms: Date.parse(show.audio.created_at),
-          duration: show.audio.duration_hms,
-          title: show.title,
-          audio: show.audio.encodings[0].http_file_path,
-          artists: show.title.trim().replace("and", "").split(",")
-        }        
-      })
+        if (!exclusions.includes(show.audio.id)) {
+          let data = {
+            id: show.audio.id,
+            date: show.audio.created_at,
+            date_ms: Date.parse(show.audio.created_at),
+            duration: show.audio.duration_hms,
+            title: show.title,
+            audio: show.audio.encodings[0].http_file_path,
+            artists: show.title.split(","),
+            last_updated: new Date(Date.now())
+          }
+          data.artists = data.artists.map(artist => {
+            artist = artist.trim()
+            if (artist.indexOf("and") === 0) artist = artist.slice(4)
+            return artist
+          })
+          console.log(`Adding ${data.title}`)
+          return data
+        }
+      }).filter(show => show) // filter nulls
       resolve(shows)
     })
   },
@@ -47,7 +64,7 @@ module.exports = {
     })
   },
   write(shows) {
-    let [newShows, oldShows] = shows    
+    let [newShows, oldShows] = shows
     if (!oldShows) {
       createFileAndDirectory(showFile, showPath, JSON.stringify(newShows))
       return
@@ -56,7 +73,7 @@ module.exports = {
       let updatedShows = [...oldShows]
       let ids = oldShows.map(show => show.id)
       newShows.forEach(show => {
-        if (!ids.includes(show.id)) updatedShows.push(show)   
+        if (!ids.includes(show.id)) updatedShows.push(show)
       })
       updatedShows = sort(updatedShows)
       createFileAndDirectory(showFile, showPath, JSON.stringify(updatedShows))
